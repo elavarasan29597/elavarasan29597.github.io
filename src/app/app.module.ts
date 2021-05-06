f4b0185adb340fdf627b9004a53599a7c1cf515b
“elavarasan29597” <elavarasan29597@gmail.com>
2021-05-06 17:55:09 +0530

    change id

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -78,7 +78,7 @@ import { TestComponent } from './components/test/test.component';
             id: GoogleLoginProvider.PROVIDER_ID,
             provider: new GoogleLoginProvider(
               // '1080951798608-mmm2qmgjvqqnqm10kmpp4qi6mukjc0sc.apps.googleusercontent.com'
-              '61095316339-kcba3eek6to8tfan7tqov8k6jlcm26ie.apps.googleusercontent.com'
+              '514371455727-5qs6g2kkppa902v6ujte1hi3e0mg95er.apps.googleusercontent.com'
             )
           },
           {
